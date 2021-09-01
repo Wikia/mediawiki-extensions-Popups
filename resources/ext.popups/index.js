c3f4d537044b94fcab0d38d0ec17fbd25879432a
Karol Tatała <ktatala@wikia-inc.com>
2021-09-01 12:07:46 +0200

    Disable experiment for wikis with wgEnablePopups

--- resources/ext.popups/index.js
+++ resources/ext.popups/index.js
@@ -1,6 +1,10 @@
 function loadPopups() {
 	// load popups only if the extension is enabled in WikiConfig or user is in experiment group
 	if (window.pathfinderPopupsExtVariant || window.wgEnablePopups) {
+		if (window.wgEnablePopups) {
+			// disable the experiment for wikis that have Popups extensions disabled explicitly
+			window.pathfinderPopupsExtVariant = undefined;
+		}
 		mw.loader.using( 'ext.popups.main' );
 	}
 }
